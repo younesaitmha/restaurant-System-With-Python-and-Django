cfa9c180eb682884060128792bbc589d9c21eea5
younes ait m'ha <younesaitmha@gmail.com>
2021-03-25 22:33:32 +0100

    add urls for meals app

---
+++ meals/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+
+app_name = 'meals'
+
+urlpatterns = [
+    path('', views.meal_list, name='meal_list'),
+    path('<slug:slug>', views.meal_detail, name='meal_detail'),
+]
