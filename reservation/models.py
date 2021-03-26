2978338f0b79ba022befbcc4ac476bbe16182468
younes ait m'ha <younesaitmha@gmail.com>
2021-03-26 16:39:56 +0100

    finish the reservation app

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Reservation(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.IntegerField()
+    number_of_persons = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+
+    class Meta:
+        verbose_name = 'reservation'
+        verbose_name_plural = 'reservations'
+
+    def __str__(self) -> str:
+        return self.name
--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Reservation
+from .forms import ReserveTableForm
 
-# Create your views here.
+
+def reserve_table(request):
+    reserve_form = ReserveTableForm()
+
+    if request.method == 'POST' :
+        reserve_form = ReserveTableForm(request.POST)
+
+        if reserve_form.is_valid():
+            reserve_form.save()
+
+    context = {'form' : reserve_form}
+
+    return render(request , 'reservation.html' , context)
