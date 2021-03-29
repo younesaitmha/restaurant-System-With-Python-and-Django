e42d8e12a9272d85ccf0bbb4c1fa3b3f6579d45c
younes ait m'ha <younesaitmha@gmail.com>
2021-03-29 09:22:21 +0100

    customize admin panel for contact app

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,19 @@
+from .models import Category, Post , Comment
 from django.contrib import admin
-from .models import Post, Category, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
 
-admin.site.register(Post)
+
+
+class PostAdmin(SummernoteModelAdmin , admin.ModelAdmin):  # instead of ModelAdmin
+    summernote_fields = '__all__'
+    list_display = ['title' ,'author', 'category', 'created']
+    search_fields = ['title', 'content' ]
+    list_filter = ('category','tags')
+
+
+
+
+admin.site.register(Post , PostAdmin)
 admin.site.register(Category)
 admin.site.register(Comment)
