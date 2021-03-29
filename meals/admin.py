735d79eac3e0c9cd12315066e10afb56fd2b7ca1
younes ait m'ha <younesaitmha@gmail.com>
2021-03-29 09:18:29 +0100

    costumize admin panel for meals app

--- meals/admin.py
+++ meals/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Meal , Category
 
-# Register your models here.
 
-from .models import Meal, Category
+class MealAdmin(SummernoteModelAdmin , admin.ModelAdmin):  # instead of ModelAdmin
+    summernote_fields = '__all__'
+    list_display = ['name' ,'preparation_time', 'people', 'price']
+    search_fields = ['name', 'description' ]
+    list_filter = ('category','people')
 
-admin.site.register(Meal)
+admin.site.register(Meal , MealAdmin)
 admin.site.register(Category)
