9c4865e25488b152c7d3e15a9668b15b459d4b60
younes ait m'ha <younesaitmha@gmail.com>
2021-03-29 09:23:24 +0100

    update contact app

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
 
-# Create your views here.
+from .forms import ContactForm
+
+
+
+def send_email(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+
+    else:
+        form = ContactForm()
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            phone = form.cleaned_data['phone']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+
+            try :
+                send_mail(name, phone, message,from_email, ['younesmathss@gmail.com'])
+
+            except BadHeaderError:
+                return HttpResponse('invalid header')
+
+            return HttpResponseRedirect('contact:send_success')
+
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'contact.html', context)
+
+
+def send_success(request):
+     return HttpResponse('thanks you for your email ^_^')
